728002c9408b771e2f75531f01b7f58020cf8215
marker1k <marker1k@mail.ru>
2018-08-22 19:00:31 +0300

    pkuz fixes

--- conf.js
+++ conf.js
@@ -27,6 +27,9 @@ var biddersMap = {
         main.removeFromArray(biddersMap.biddersMapUse, bidder);
         if (Object.values(biddersMap.campaignIdUsed).indexOf(campaign) == -1) {
           biddersMap.campaignIdUsed[bidder] = campaign;
+          if (document.getElementById("generateError").style.visibility == "visible") {
+            document.getElementById("generateError").style.visibility = "hidden";
+          }
           if (document.getElementById("addBiddersMapUiElement").disabled == true) {
             document.getElementById("addBiddersMapUiElement").disabled = false;
           }
@@ -117,20 +120,28 @@ var AdUnits = {
           elem.parentNode.parentNode.parentNode.getElementsByTagName("button")[0].disabled = false;
           document.getElementById("adUnitError").style.visibility = "hidden";
         }
+        if (document.getElementById("sameContainer").style.visibility == "visible") {
+          document.getElementById("sameContainer").style.visibility = "hidden";
+          for (var i = 0; i < generateButtons.length; i += 1) {
+            generateButtons[i].disabled = false;
+          }
+        }
       } else {
         elem.parentNode.parentNode.parentNode.getElementsByTagName("button")[0].disabled = true;
-        document.getElementById("adUnitError").innerHTML = "ID контейнеров должны быть уникальными";
-        document.getElementById("adUnitError").style.visibility = "visible";
+        document.getElementById("sameContainer").innerHTML = "ID контейнеров должны быть уникальными";
+        document.getElementById("sameContainer").style.visibility = "visible";
+        for (var i = 0; i < generateButtons.length; i += 1) {
+          generateButtons[i].disabled = true;
+        }
       }
     }
   },
   addUi: function(elem) {
-    AdUnits.count += 1;
     // console.log(elem.previousElementSibling);
     AdUnits.adUnitsUsed.push({});
     var ad_units = document.getElementById("adUnitsInner");
     units_element = document.createElement("P");
-    units_element_inner = '<div class="units_row" id=' + AdUnits.count + '>' +
+    units_element_inner = '<div class="units_row">' +
       '<div class="delete adUnitDelete" onclick="AdUnits.removeUnit(this);this.parentNode.parentNode.remove();" id="adUnitsCloseButton"></div>' +
       '<div id="adUnitError">ERROR</div>' +
       '<p style="font-size: 15px;color: gray;">ID контейнера ADFOX:</p>' +
@@ -155,8 +166,14 @@ var AdUnits = {
   removeUnit: function(elem) {
     var id = elem.parentNode.id;
     AdUnits.adUnitsUsed.splice(id - 1, 1);
+    if (document.getElementById("sameContainer").style.visibility == "visible") {
+      document.getElementById("sameContainer").style.visibility = "hidden";
+    }
   },
   addAdUnitsUiElement: function(elem) {
+    if (document.getElementById("fillAdUnits").style.visibility == "visible") {
+      document.getElementById("fillAdUnits").style.visibility = "hidden"
+    }
     if (document.getElementsByClassName("bidder_map").length === 0) {
       document.getElementById("mainError").innerHTML = "Сначала добавьте биддеров";
       document.getElementById("mainError").style.visibility = "visible";
@@ -170,63 +187,64 @@ var AdUnits = {
   updateSizes: function(elem) {
     clearTimeout(this.sizesTimeout);
     this.sizesTimeout = setTimeout(function() {
-      if (document.getElementById("adUnitError").style.visibility == "visible") {
-        document.getElementById("adUnitError").style.visibility = "hidden";
+      if (document.getElementById("sizesError").style.visibility == "visible") {
+        document.getElementById("sizesError").style.visibility = "hidden";
       }
       var index = main.getPreviousSiblings(elem.parentNode.parentNode.parentNode).length;
       if (/\[(.*\S.*)\]/.test(elem.value)) {
         AdUnits.adUnitsUsed[index].sizes = JSON.parse("[" + elem.value + "]");
         elem.parentNode.parentNode.getElementsByTagName("button")[0].disabled = false;
-        if (document.getElementById("adUnitError").style.visibility == "visible") {
-          document.getElementById("adUnitError").style.visibility = "hidden";
+        if (document.getElementById("sizesError").style.visibility == "visible") {
+          document.getElementById("sizesError").style.visibility = "hidden";
         }
       } else {
-        document.getElementById("adUnitError").innerHTML = "Должен быть не пустой массив";
-        document.getElementById("adUnitError").style.visibility = "visible";
+        document.getElementById("sizesError").innerHTML = "Должен быть не пустой массив";
+        document.getElementById("sizesError").style.visibility = "visible";
       }
     }, 200);
   },
   updateBidderElement: function(elem) {
-      elem.parentNode.parentNode.getElementsByClassName("bidder")[0].disabled = true;
-      var index = main.getPreviousSiblings(elem.parentNode.parentNode.parentNode.parentNode.parentNode).length;
-      var bidder = elem.parentNode.parentNode.getElementsByClassName("bidder")[0].value;
-      var id = elem.value;
-      // if (AdUnits.adUnitsUsed[index].bids == undefined) {
-      //   AdUnits.adUnitsUsed[index].bids = [];
-      // }
-      // Тут сначала нужно сделать проверку нет ли уже биддера в adunite main.search(AdUnits.adUnitsUsed[0].bids, 'criteo', 'bidder')
-      // И нет ли в другом AdUnit такого id. Тоже через main.search
-      // Если есть, то просто обновить значение у него
-      var found = AdUnits.adUnitsUsed.find(function(code) {
-        return code.bids.some(function(bid) {
-          return bid.bidder === bidder && bid.params.placementId === id;
-        });
+    elem.parentNode.parentNode.getElementsByClassName("bidder")[0].disabled = true;
+    var index = main.getPreviousSiblings(elem.parentNode.parentNode.parentNode.parentNode.parentNode).length;
+    var bidder = elem.parentNode.parentNode.getElementsByClassName("bidder")[0].value;
+    var id = elem.value;
+    // if (AdUnits.adUnitsUsed[index].bids == undefined) {
+    //   AdUnits.adUnitsUsed[index].bids = [];
+    // }
+    // Тут сначала нужно сделать проверку нет ли уже биддера в adunite main.search(AdUnits.adUnitsUsed[0].bids, 'criteo', 'bidder')
+    // И нет ли в другом AdUnit такого id. Тоже через main.search
+    // Если есть, то просто обновить значение у него
+    var found = AdUnits.adUnitsUsed.find(function(code) {
+      return code.bids.some(function(bid) {
+        return bid.bidder === bidder && bid.params.placementId === id;
       });
-      if (found == undefined) {
-        var bidderExists = main.search(AdUnits.adUnitsUsed[index].bids, bidder, "bidder");
-        if (bidderExists != undefined) {
-          if (document.getElementById("adUnitError").style.visibility == "visible") {
-            document.getElementById("adUnitError").style.visibility = "hidden";
-            for (var i = 0; i < generateButtons.length; i += 1) {
-              generateButtons[i].disabled = false;
-            }
+    });
+    if (found == undefined) {
+      var bidderExists = main.search(AdUnits.adUnitsUsed[index].bids, bidder, "bidder");
+      if (bidderExists != undefined) {
+        if (document.getElementById("adUnitError").style.visibility == "visible") {
+          document.getElementById("adUnitError").style.visibility = "hidden";
+          for (var i = 0; i < generateButtons.length; i += 1) {
+            generateButtons[i].disabled = false;
           }
-          var position = AdUnits.adUnitsUsed[index].bids.indexOf(bidderExists);
-          AdUnits.adUnitsUsed[index].bids[position].params.placementId = id;
-        } else {
-          if (bidder == "adriver" || bidder == "betweenDigital") {
-            var sizes = elem.parentNode.parentNode.parentNode.parentNode.getElementsByClassName("sizes")[0].value;
-            if (sizes == "") {
-              AdUnits.adUnitsUsed[index].bids.push({
-                bidder: bidder,
-                params: {
-                  placementId: id
-                }
-              });
-              elem.parentNode.parentNode.parentNode.parentNode.getElementsByTagName("button")[0].disabled = true;
-              document.getElementById("adUnitError").innerHTML = "Заполните размеры";
-              document.getElementById("adUnitError").style.visibility = "visible";
-            } else {
+        }
+        var position = AdUnits.adUnitsUsed[index].bids.indexOf(bidderExists);
+        AdUnits.adUnitsUsed[index].bids[position].params.placementId = id;
+      } else {
+        if (bidder == "adriver" || bidder == "betweenDigital") {
+          var sizes = elem.parentNode.parentNode.parentNode.parentNode.getElementsByClassName("sizes")[0].value;
+          if (sizes == "") {
+            AdUnits.adUnitsUsed[index].bids.push({
+              bidder: bidder,
+              params: {
+                placementId: id
+              }
+            });
+            elem.parentNode.parentNode.parentNode.parentNode.getElementsByTagName("button")[0].disabled = true;
+            document.getElementById("sizesError").innerHTML = "Заполните размеры";
+            document.getElementById("sizesError").style.visibility = "visible";
+          } else {
+            if (main.alphanumeric(id)) {
               AdUnits.adUnitsUsed[index].bids.push({
                 bidder: bidder,
                 sizes: [sizes],
@@ -234,35 +252,42 @@ var AdUnits = {
                   placementId: id
                 }
               });
+            } else {
+              console.log("Wrong letters");
             }
+          }
+        } else {
+          // if (document.getElementById("adUnitError").style.visibility == "visible") {
+          //   document.getElementById("adUnitError").style.visibility = "hidden";
+          // }
+          // AdUnits.bidderState.push({});
+          // AdUnits.bidderState[index - 1][bidder] = id;
+          if (main.alphanumeric(id) == false) {
+            console.log("Wrong letters");
           } else {
-            // if (document.getElementById("adUnitError").style.visibility == "visible") {
-            //   document.getElementById("adUnitError").style.visibility = "hidden";
-            // }
-            // AdUnits.bidderState.push({});
-            // AdUnits.bidderState[index - 1][bidder] = id;
             AdUnits.adUnitsUsed[index].bids.push({
               bidder: bidder,
               params: {
                 placementId: id
               }
             });
-
           }
-        }
-      } else {
-        console.log(elem.parentNode.parentNode.getElementsBy);
-        document.getElementById("adUnitError").innerHTML = bidder + " уже использует такой placementId";
-        document.getElementById("adUnitError").style.visibility = "visible";
-        for (var i = 0; i < generateButtons.length; i += 1) {
-          generateButtons[i].disabled = true;
+
         }
       }
-      elem.parentNode.parentNode.getElementsByClassName("bidder")[0] = true;
+    } else {
+      console.log(elem.parentNode.parentNode.getElementsBy);
+      document.getElementById("adUnitError").innerHTML = bidder + " уже использует такой placementId";
+      document.getElementById("adUnitError").style.visibility = "visible";
+      for (var i = 0; i < generateButtons.length; i += 1) {
+        generateButtons[i].disabled = true;
+      }
+    }
+    elem.parentNode.parentNode.getElementsByClassName("bidder")[0] = true;
   },
   addBidder: function(elem) {
     if (generateButtons[0].disabled && generateButtons[1].disabled) {
-      for (var i = 0; i < generateButtons.length; i+=1) {
+      for (var i = 0; i < generateButtons.length; i += 1) {
         generateButtons[i].disabled = false;
       }
     }
@@ -314,6 +339,12 @@ var AdUnits = {
       elem.parentNode.parentNode.parentNode.getElementsByClassName("sizes")[0].value = "";
       elem.parentNode.parentNode.parentNode.getElementsByClassName("sizes")[0].style.display = "none";
       delete AdUnits.adUnitsUsed[index].sizes;
+      if (elem.parentNode.parentNode.parentNode.querySelector("#addBidder").disabled == true) {
+        elem.parentNode.parentNode.parentNode.querySelector("#addBidder").disabled = false;
+      }
+      if (document.getElementById("sizesError").style.visibility == "visible") {
+        document.getElementById("sizesError").style.visibility = "hidden";
+      }
     }
   }
 }
@@ -384,12 +415,20 @@ var main = {
     }
   },
   showSuccess: function() {
-    document.getElementById("adUnitError").innerHTML = "Код скопирован в буфер обмена";
-    document.getElementById("adUnitError").style.backgroundColor = "green";
-    document.getElementById("adUnitError").style.visibility = "visible";
-    setTimeout(function(){
-      document.getElementById("adUnitError").style.visibility = "hidden";
+    document.getElementById("timeoutError").innerHTML = "Код скопирован в буфер обмена";
+    document.getElementById("timeoutError").style.backgroundColor = "green";
+    document.getElementById("timeoutError").style.visibility = "visible";
+    setTimeout(function() {
+      document.getElementById("timeoutError").style.visibility = "hidden";
     }, 1500)
+  },
+  alphanumeric: function(inputtxt) {
+    var letterNumber = /^[0-9a-zA-Z]+$/;
+    if (inputtxt.match(letterNumber)) {
+      return true;
+    } else {
+      return false;
+    }
   },
   highlightCode: function() {
     if (document.getElementsByClassName("CodeMirror").length > 0) {
@@ -407,21 +446,52 @@ var main = {
     });
   },
   createBiddersMap: function() {
-    return 'const adfoxBiddersMap = ' + JSON.stringify(biddersMap.campaignIdUsed, null, 4) + ';';
+    return 'var adfoxBiddersMap = ' + JSON.stringify(biddersMap.campaignIdUsed, null, 4) + ';';
   },
-  makeInstall: function() {
+  make: function() {
     var tail = 'var userTimeout = ' + timeoutInput.timeout + ';' + '\n' +
       'window.YaHeaderBiddingSettings = {' + '\n' +
       '    biddersMap: adfoxBiddersMap,' + '\n' +
       '    adUnits: adUnits,' + '\n' +
       '    timeout: userTimeout' + '\n' +
       '};' + '\n' + '</script>';
-    var loader = '<script src="https://yastatic.net/pcode/adfox/loader.js" crossorigin="anonymous"><\/script>';
+    var loader = '<script src="https://yastatic.net/pcode/adfox/loader.js" crossorigin="anonymous"' + as + '><\/script>';
+    if (document.getElementById("as").checked) {
+      var loader = '<script async src="https://yastatic.net/pcode/adfox/loader.js" crossorigin="anonymous"><\/script>';
+    } else {
+      var loader = '<script src="https://yastatic.net/pcode/adfox/loader.js" crossorigin="anonymous"><\/script>';
+    }
     document.getElementById('result_textarea').value = '<script async src="https://yastatic.net/pcode/adfox/header-bidding.js"><\/script>' + '\n' + '<script>' + '\n';
     document.getElementById('result_textarea').value += main.createBiddersMap();
     document.getElementById('result_textarea').value += '\n' + 'var adUnits = ' + JSON.stringify(AdUnits.adUnitsUsed, null, 4) + ';' + '\n' + tail;
     document.getElementById('result_textarea').value += '\n' + loader;
     main.highlightCode();
-    // document.getElementById("generate").disabled = true;
+  },
+  makeInstall: function() {
+    var bidders = [];
+    AdUnits.adUnitsUsed.forEach(function(element) {
+      element.bids.forEach(function(item) {
+        if (bidders.indexOf(item.bidder) == -1) {
+          if (!(item.bidder in biddersMap.campaignIdUsed)) {
+            bidders.push(item.bidder);
+          }
+        }
+      });
+    });
+    if (bidders.length == 0) {
+      if (Object.keys(biddersMap.campaignIdUsed).length === 0 && biddersMap.campaignIdUsed.constructor === Object) {
+        document.getElementById("generateError").innerHTML = "Заполните Bidders Map";
+        document.getElementById("generateError").style.visibility = "visible";
+      } else if (AdUnits.adUnitsUsed.length == 0) {
+        document.getElementById("fillAdUnits").innerHTML = "Заполните Ad Units";
+        document.getElementById("fillAdUnits").style.visibility = "visible";
+      } else {
+        main.make();
+      }
+    } else {
+      document.getElementById("generateError").innerHTML = "В Bidders Map не добавлены ID кампаний для " + JSON.stringify(bidders).slice(2, -2);;
+      document.getElementById("generateError").style.visibility = "visible";
+    }
+
   }
 };
